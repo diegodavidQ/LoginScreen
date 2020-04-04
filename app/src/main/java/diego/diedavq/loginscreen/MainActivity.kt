cdf004435f939bbb531a9f8a0ebfea4531c43bf4
Diego David <diegodavidq@outlook.com>
2020-04-04 18:17:46 -0500

    Initial commit Login Screen

---
+++ app/src/main/java/diego/diedavq/loginscreen/MainActivity.kt
@@ -0,0 +1,13 @@
+package diego.diedavq.loginscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //https://iconos8.es/
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
